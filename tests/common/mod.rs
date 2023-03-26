2718e71a636163011893c233fa358e34fb6e5de5
Maddux <maddux.macdonald@gmail.com>
2023-03-26 15:11:59 -0400

    Fixed encryption. Started on test case creation

---
+++ tests/common/mod.rs
@@ -0,0 +1,6 @@
+// Using this as an example of how testing could be done
+// This module provides an example of a setup that could be called and run from other tests
+
+fn setup() {
+    // This function can do setup for any functions that call it
+}
